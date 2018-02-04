c01f2e6b1f50ce12fd2d35e8502290e735bf4d88
Carlos <ceduardosa4@gmail.com>
2018-02-04 23:00:27 +0000

    Added Volume control

--- Automatic_Volume_Control/Automatic_Volume_Control/MainWindow.xaml.cs
+++ Automatic_Volume_Control/Automatic_Volume_Control/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using AudioSwitcher.AudioApi.CoreAudio;
+
 
 namespace Automatic_Volume_Control
 {
@@ -20,9 +23,35 @@ namespace Automatic_Volume_Control
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+        double current_volume = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            current_volume=defaultPlaybackDevice.Volume;
+        }
+
+        private void Vol_up_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Current Volume:" + defaultPlaybackDevice.Volume);
+            current_volume = current_volume + 10;
+            defaultPlaybackDevice.Volume = current_volume;
+        }
+
+        private void Vol_Dwn_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Current Volume:" + defaultPlaybackDevice.Volume);
+            current_volume = current_volume - 10;
+            defaultPlaybackDevice.Volume = current_volume;
+        }
+
+        private void Mute_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Current Volume:" + defaultPlaybackDevice.Volume);
+            current_volume = 0;
+            defaultPlaybackDevice.Volume = current_volume;
         }
     }
 }
